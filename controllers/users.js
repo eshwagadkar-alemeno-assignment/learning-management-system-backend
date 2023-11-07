87e24088fe60d30b9634212a4144d5665c104aae
eshwagadkar <eshwagadkar007@gmail.com>
2023-11-07 15:49:25 +0530

    logic to register a user

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 const HttpError = require('../models/http-error');
 
@@ -7,12 +8,38 @@ const User = require('../models/users');
 const signUp = async (req, res, next) => {
 
     const errors = validationResult(req);
+    const secret = process.env.SECRET_KEY;
 
     if(!errors.isEmpty()){
         const error = new HttpError('Please fill in the required fields', 422, false);
         return next(error);  
     }
+
+    const { name, email, password, phone, isAdmin, } = req.body;
     
+    let userExists; 
+    try {
+        userExists = await User.findOne({ email });
+    } catch (err) {
+        const error = new HttpError('Registration failed', 500, false);
+        return next(error);
+    }
+
+    if(userExists){
+        const error = new HttpError('User exists already, Please Login instead', 422, false);
+        return next(error);
+    }
+
+    const createdUser = new User({ name, email, password, phone, isAdmin });
+
+    try{
+        await createdUser.save();
+        const token = jwt.sign({ userId: createdUser._id }, secret, { expiresIn: '1d'} );
+        res.status(201).json( {  user: createdUser.toObject({ getters: true }), token } );
+    } catch (err) {
+        const error = new HttpError('User Registeration Failed', 500, false);
+        return next(error);
+    }
 }
 
 exports.signUp = signUp;
