68a10e595c10cdc3d450072c081359a7021edfe5
eshwagadkar <eshwagadkar007@gmail.com>
2023-11-07 15:46:35 +0530

    init express jwt middleware

---
+++ middlewares/jwt.js
@@ -0,0 +1,32 @@
+const { expressjwt: jwt } = require('express-jwt');
+
+function authJwt() {
+    const secret = process.env.SECRET_KEY;
+
+    return jwt({
+        secret,
+        algorithms: ['HS256'],
+        isRevoked: isRevoked,
+    }).unless({
+        path : [
+            // { url: /\/public\/uploads(.*)/ , methods: [ 'GET', 'OPTIONS'] },
+            // { url: /\/api\/v1\/products(.*)/ , methods: [ 'GET', 'OPTIONS'] },
+            // { url: /\/api\/v1\/categories(.*)/ , methods: [ 'GET', 'OPTIONS'] },
+            '/api/v1/users/register',
+            '/api/v1/users/login',
+        ]
+    })
+}
+
+async function isRevoked(req, payload) {
+    
+    // if our user makes a authorized api call and is not admin then reject the token
+    if(payload.payload.isAdmin == false){
+        // not a admin : reject the token
+        return true;
+    } 
+    // admin
+    return false;
+}
+
+module.exports = authJwt;
