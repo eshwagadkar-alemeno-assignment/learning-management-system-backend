7bee35a017ae29493aa0663ae6d7ea81228ce83d
eshwagadkar <eshwagadkar007@gmail.com>
2023-11-07 19:45:50 +0530

    middleware for error handling

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const usersRoute = require('./routes/users');
 
 // Middleware Imports
 const authJwt = require('./middlewares/jwt');
-const errorHandler = require('./middlewares/requireAuth');
 
 const app = express();
 
@@ -39,6 +38,34 @@ app.use(`${api}/course`, courseRoute);
 
 // Registering the imported routes as a middleware
 app.use(`${api}/users`, usersRoute);
+
+// Middleeware that handles unsupported routes
+app.use((error, res, next) => {
+    const err = new HttpError('Could not find this route', 404, false );
+    throw err;
+})
+
+// Middleware for error handling
+app.use((err, req, res, next) => {
+
+    // JWT Authentication Error
+    if(err.name === 'UnauthorizedError'){
+        return res.status(500).json({ message: 'The user is not authorized', success: false });
+    }
+
+     // Validation Error
+    if(err.name === 'ValidationError'){
+       return res.status(401).json({ message: err, success: false })
+    }
+
+    if(res.headerSent){
+        return next(err);
+    }
+
+    // Default to General: 500 Server Error
+     return res.status(err.code || 500).json({ message: err.message || 'An unknown error occurred!', success: err.success });
+
+})
 
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
